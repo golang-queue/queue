c26d79533559f00ab3bf93259ac0d1bf8c04765f
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-08-01 14:35:47 +0800

    chore(queue): update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ _example/simple/example02/go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.16
+
+require github.com/appleboy/queue v0.0.5-0.20210801061748-f4074cb5fc30
---
+++ _example/simple/example02/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/appleboy/queue"
+	"github.com/appleboy/queue/simple"
+)
+
+func main() {
+	taskN := 100
+	rets := make(chan string, taskN)
+
+	// define the worker
+	w := simple.NewWorker(
+		simple.WithQueueNum(taskN),
+	)
+
+	// define the queue
+	q, err := queue.NewQueue(
+		queue.WithWorkerCount(5),
+		queue.WithWorker(w),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// start the five worker
+	q.Start()
+
+	// assign tasks in queue
+	for i := 0; i < taskN; i++ {
+		go func(i int) {
+			q.QueueTask(func(ctx context.Context) error {
+				rets <- fmt.Sprintf("Hi appleboy, handle the job: %02d", +i)
+				return nil
+			})
+		}(i)
+	}
+
+	// wait until all tasks done
+	for i := 0; i < taskN; i++ {
+		fmt.Println("message:", <-rets)
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	// shutdown the service and notify all the worker
+	q.Shutdown()
+	// wait all jobs are complete.
+	q.Wait()
+}
