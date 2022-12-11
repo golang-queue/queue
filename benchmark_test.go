5a517458ad60cb3ae2f722c306de645f73cf5da6
Bo-Yi.Wu <appleboy.tw@gmail.com>
2022-12-11 21:57:52 +0800

    chore(benchmark): add performance benchmark.
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,6 +3,10 @@ package queue
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/golang-queue/queue/core"
+	"github.com/golang-queue/queue/job"
 )
 
 func BenchmarkQueueTask(b *testing.B) {
@@ -25,5 +29,51 @@ func BenchmarkQueue(b *testing.B) {
 	defer q.Release()
 	for n := 0; n < b.N; n++ {
 		_ = q.Queue(m)
+	}
+}
+
+func BenchmarkConsumerPayload(b *testing.B) {
+	b.ReportAllocs()
+
+	task := &job.Message{
+		Timeout: 100 * time.Millisecond,
+		Payload: []byte(`{"timeout":3600000000000}`),
+	}
+	w := NewConsumer(
+		WithFn(func(ctx context.Context, m core.QueuedMessage) error {
+			return nil
+		}),
+	)
+
+	q, _ := NewQueue(
+		WithWorker(w),
+	)
+
+	for n := 0; n < b.N; n++ {
+		_ = q.run(task)
+	}
+}
+
+func BenchmarkConsumerTask(b *testing.B) {
+	b.ReportAllocs()
+
+	task := &job.Message{
+		Timeout: 100 * time.Millisecond,
+		Task: func(_ context.Context) error {
+			return nil
+		},
+	}
+	w := NewConsumer(
+		WithFn(func(ctx context.Context, m core.QueuedMessage) error {
+			return nil
+		}),
+	)
+
+	q, _ := NewQueue(
+		WithWorker(w),
+	)
+
+	for n := 0; n < b.N; n++ {
+		_ = q.run(task)
 	}
 }
