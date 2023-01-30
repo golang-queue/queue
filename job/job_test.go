e64d4a03ea08b6a65a761b09041512aa50fa185a
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-01-30 17:05:20 +0800

    chore(job): serialize a struct to bytes (#103)
    BenchmarkEncode
    BenchmarkEncode/JSON
    BenchmarkEncode/JSON-2                               	 6139400	       189.3 ns/op	      96 B/op	       1 allocs/op
    BenchmarkEncode/JSON-2                               	 6316482	       189.1 ns/op	      96 B/op	       1 allocs/op
    BenchmarkEncode/UnsafeCast
    BenchmarkEncode/UnsafeCast-2                         	1000000000	         0.5096 ns/op	       0 B/op	       0 allocs/op
    BenchmarkEncode/UnsafeCast-2                         	1000000000	         0.5098 ns/op	       0 B/op	       0 allocs/op
    BenchmarkDecode
    BenchmarkDecode/JSON
    BenchmarkDecode/JSON-2                               	 3469248	       344.1 ns/op	      99 B/op	       2 allocs/op
    BenchmarkDecode/JSON-2                               	 3480768	       343.9 ns/op	      99 B/op	       2 allocs/op
    BenchmarkDecode/UnsafeCast
    BenchmarkDecode/UnsafeCast-2                         	1000000000	         0.4021 ns/op	       0 B/op	       0 allocs/op
    BenchmarkDecode/UnsafeCast-2                         	1000000000	         0.4021 ns/op	       0 B/op	       0 allocs/op

--- job/job_test.go
+++ job/job_test.go
@@ -1,1 +1,53 @@
 package job
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTaskBinaryEncode(t *testing.T) {
+	m := NewTask(func(context.Context) error {
+		return nil
+	},
+		AllowOption{
+			RetryCount: Int64(100),
+			RetryDelay: Time(30 * time.Millisecond),
+			Timeout:    Time(3 * time.Millisecond),
+		},
+	)
+
+	out := Decode(Encode(m))
+
+	assert.Equal(t, int64(100), out.RetryCount)
+	assert.Equal(t, 30*time.Millisecond, out.RetryDelay)
+}
+
+type mockMessage struct {
+	message string
+}
+
+func (m mockMessage) Bytes() []byte {
+	return []byte(m.message)
+}
+
+func TestMessageBinaryEncode(t *testing.T) {
+	m := NewMessage(&mockMessage{
+		message: "foo",
+	},
+		AllowOption{
+			RetryCount: Int64(100),
+			RetryDelay: Time(30 * time.Millisecond),
+			Timeout:    Time(3 * time.Millisecond),
+		},
+	)
+
+	m.Encode()
+	out := Decode(m.Bytes())
+
+	assert.Equal(t, int64(100), out.RetryCount)
+	assert.Equal(t, 30*time.Millisecond, out.RetryDelay)
+	assert.Equal(t, "foo", string(out.Payload))
+}
