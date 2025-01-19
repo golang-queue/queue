da26ae2d9402571f544dc16d43bc6bc8f80a4bcb
appleboy <appleboy.tw@gmail.com>
2025-01-19 17:09:27 +0800

    refactor: switch to msgpack for message encoding/decoding
    - Add `github.com/vmihailenco/msgpack/v5` and `github.com/vmihailenco/tagparser/v2` dependencies to `go.mod`
    - Remove `unsafe` import from `job.go`
    - Add `msgpack` tags to `Message` struct fields
    - Remove `movementSize` constant and its usage
    - Replace unsafe pointer encoding/decoding with `msgpack` encoding/decoding in `Encode` and `Decode` functions
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/appleboy/com v0.2.1
 	github.com/jpillora/backoff v1.0.0
 	github.com/stretchr/testify v1.10.0
+	github.com/vmihailenco/msgpack/v5 v5.4.1
 	go.uber.org/goleak v1.2.1
 	go.uber.org/mock v0.5.0
 )
@@ -14,5 +15,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- job/job.go
+++ job/job.go
@@ -3,9 +3,9 @@ package job
 import (
 	"context"
 	"time"
-	"unsafe"
 
 	"github.com/golang-queue/queue/core"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
 // TaskFunc is the task function
@@ -13,49 +13,45 @@ type TaskFunc func(context.Context) error
 
 // Message describes a task and its metadata.
 type Message struct {
-	Task TaskFunc `json:"-"`
+	Task TaskFunc `json:"-" msgpack:"-"`
 
 	// Timeout is the duration the task can be processed by Handler.
 	// zero if not specified
 	// default is 60 time.Minute
-	Timeout time.Duration `json:"timeout"`
+	Timeout time.Duration `json:"timeout" msgpack:"timeout"`
 
 	// Payload is the payload data of the task.
-	Payload []byte `json:"body"`
+	Payload []byte `json:"body" msgpack:"body"`
 
 	// RetryCount set count of retry
 	// default is 0, no retry.
-	RetryCount int64 `json:"retry_count"`
+	RetryCount int64 `json:"retry_count" msgpack:"retry_count"`
 
 	// RetryDelay set delay between retry
 	// default is 100ms
-	RetryDelay time.Duration `json:"retry_delay"`
+	RetryDelay time.Duration `json:"retry_delay" msgpack:"retry_delay"`
 
 	// RetryFactor is the multiplying factor for each increment step.
 	//
 	// Defaults to 2.
-	RetryFactor float64 `json:"retry_factor"`
+	RetryFactor float64 `json:"retry_factor" msgpack:"retry_factor"`
 
 	// Minimum value of the counter.
 	//
 	// Defaults to 100 milliseconds.
-	RetryMin time.Duration `json:"retry_min"`
+	RetryMin time.Duration `json:"retry_min" msgpack:"retry_min"`
 
 	// Maximum value of the counter.
 	//
 	// Defaults to 10 seconds.
-	RetryMax time.Duration `json:"retry_max"`
+	RetryMax time.Duration `json:"retry_max" msgpack:"retry_max"`
 
 	// Jitter eases contention by randomizing backoff steps
-	Jitter bool `json:"jitter"`
+	Jitter bool `json:"jitter" msgpack:"jitter"`
 
 	// Data to save Unsafe cast
 	Data []byte
 }
-
-const (
-	movementSize = int(unsafe.Sizeof(Message{}))
-)
 
 // Bytes get internal data
 func (m *Message) Bytes() []byte {
@@ -99,10 +95,21 @@ func NewTask(task TaskFunc, opts ...AllowOption) Message {
 
 // Encode for encoding the structure
 func Encode(m *Message) []byte {
-	return (*[movementSize]byte)(unsafe.Pointer(m))[:]
+	b, err := msgpack.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+
+	return b
 }
 
 // Decode for decoding the structure
-func Decode(m []byte) *Message {
-	return (*Message)(unsafe.Pointer(&m[0]))
+func Decode(b []byte) *Message {
+	var msg Message
+	err := msgpack.Unmarshal(b, &msg)
+	if err != nil {
+		panic(err)
+	}
+
+	return &msg
 }
